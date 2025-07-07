5e93d28a5288f219dbd6137459c602e471a4989a
ayush srivastava <ayushsrivastavamail@gmail.com>
2025-07-07 23:27:54 +0530

    first commit

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,26 @@
+function MovieCard({ movie }) {
+  function onFaveriteClick() {
+    alert(`You clicked on ${movie.title}`);
+  }
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <buttion className="favite-btn" onClick={onFaveriteClick}>
+            ❤️
+          </buttion>
+        </div>
+      </div>
+      <div className="movie-details">
+        <h3>{movie.title}</h3>
+        <p>{movie.description}</p>
+        <p>Rating: {movie.rating}</p>
+        <p>Release Date: {movie.releaseDate}</p>
+        <p>Genre: {movie.genre}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const movies = [
+    { id: 1, title: "Inception", year: 2010 },
+    { id: 2, title: "Interstellar", year: 2014 },
+    { id: 3, title: "The Dark Knight", year: 2008 },
+    { id: 4, title: "Pulp Fiction", year: 1994 },
+    { id: 5, title: "The Shawshank Redemption", year: 1994 },
+    { id: 6, title: "The Godfather", year: 1972 },
+    { id: 7, title: "The Matrix", year: 1999 },
+  ];
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("---");
+  };
+  return (
+    <div ClassName="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
